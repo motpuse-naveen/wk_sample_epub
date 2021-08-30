1927feb040968834d8d8c34ad1f0a06829e495ea
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2021-08-31 00:37:52 +0530

    File conversion html to xhtml

---
+++ Sample/OEBPS/24-Blood-Therapy_FINAL/story_html5_script3.js
@@ -0,0 +1,1 @@
+if (window.globals.deviceView.isMobile) { var doc = document, loader = doc.body.querySelector('.slide-loader'); [ 1, 2, 3 ].forEach(function(n) { var d = doc.createElement('div'); d.style.backgroundColor = window.globals.preloaderColor; d.classList.add('mobile-loader-dot'); d.classList.add('dot' + n); loader.appendChild(d); }); }
---
+++ Sample/OEBPS/24-Blood-Therapy_FINAL/story_html5_script4.js
@@ -0,0 +1,3 @@
+if (window.autoSpider && window.vInterfaceObject) {
+    document.querySelector('.mobile-load-title-overlay').style.display = 'none';
+  }
